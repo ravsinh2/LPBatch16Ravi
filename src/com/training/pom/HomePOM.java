34d6f51e3d46d7267c40b5c5a9118446afdd2457
ravsinh2 <ravsinh2@in.ibm.com>
2020-01-08 21:57:43 -0500

    My First Git commit

---
+++ src/com/training/pom/HomePOM.java
@@ -0,0 +1,29 @@
+package com.training.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePOM {
+private WebDriver driver; 
+	
+	public HomePOM(WebDriver driver) {
+		this.driver = driver; 
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(name = "keyword")
+	private WebElement keywordTxtBox;
+	
+	public void enterValueInUser(String keywordValue) {
+		keywordTxtBox.sendKeys(keywordValue);
+			
+	}
+	
+	
+		
+	}
+	
+	
+
